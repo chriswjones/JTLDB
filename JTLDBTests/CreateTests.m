eff62fca3bd14e11e93930452580929052920ec2
Chris Jones <chris@workreef.com>
2012-12-14 01:38:23 -0800

    Update License on Tests

--- JTLDBTests/CreateTests.m
+++ JTLDBTests/CreateTests.m
@@ -1,9 +1,17 @@
 //
-//  CreateTests.m
-//  CreateTests
+//  Copyright 2012 JTL Systems
 //
-//  Created by Chris Jones on 11/19/12.
-//  Copyright (c) 2012 Chris Jones. All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//          http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "CreateTests.h"
@@ -22,7 +30,7 @@
 
 - (void)testCreateDatabaseWithoutSeedFile {
     NSString *dbName = @"SnowReport1.db";
-    [JDatabase openDatabaseWithName:dbName database:nil];
+    [JDatabase openDatabaseWithName:dbName seedDatabase:nil];
     NSString *dbPath = [JTLDBFolderPath() stringByAppendingPathComponent:dbName];
     STAssertTrue([[NSFileManager defaultManager] fileExistsAtPath:dbPath], @"Create database without seed file");
 }
@@ -30,7 +38,7 @@
 - (void)testCreateDatabaseWithSeedFile {
     __block BOOL inProgress = YES;
 
-    JDatabase *db = [JDatabase openDatabaseWithName:@"SnowReport2.db" database:@"seed.db"];
+    JDatabase *db = [JDatabase openDatabaseWithName:@"SnowReport2.db" seedDatabase:@"seed.db"];
 
     // Make sure the file was copied.  The seed file has 2 records
     [db.defaultConnection queueBlock:^(JConnection *connection, JOperation *operation){
--- JTLDBTests/ReadWriteTests.h
+++ JTLDBTests/ReadWriteTests.h
@@ -1,9 +1,18 @@
 //
-// Created by chrisjones on 11/19/12.
+//  Copyright 2012 JTL Systems
 //
-// To change the template use AppCode | Preferences | File Templates.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-
+//          http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 #import <SenTestingKit/SenTestingKit.h>
 
